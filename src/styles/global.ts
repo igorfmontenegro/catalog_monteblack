04ad2919d4e0879cf5428643a668aeae0e1b88a4
Igor Montenegro <117581201+igorfmontenegro@users.noreply.github.com>
2026-06-16 20:23:17 -0300

    Update global.ts
    review

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,25 +1,20 @@
 import { createGlobalStyle } from 'styled-components'
+ 
 import BrokenNoise from '../assets/broken-noise.png'
-
 export const GlobalStyles = createGlobalStyle`
-
 *{
     margin: 0;
     padding: 0;
 }
-
 #root{
-    height: 100vh;
-    width: 100vw;
+    min-height: 100vh;
+    width: 100%;
     overflow-x: hidden;
 }
-
 body{
     background-color: #000000;
     background-image: url(${BrokenNoise});
     box-sizing: border-box;
     font-family: Montserrat, sans-serif;
-    
 }
-
 `
