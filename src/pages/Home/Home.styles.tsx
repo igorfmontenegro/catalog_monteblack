bd5d1f254abbd2070e8e2a6779d9796ab5cd6f8a
Igor Montenegro <117581201+igorfmontenegro@users.noreply.github.com>
2026-06-16 21:48:30 -0300

    Update Home.styles.tsx
    Update width

--- src/pages/Home/Home.styles.tsx
+++ src/pages/Home/Home.styles.tsx
@@ -63,10 +63,6 @@ export const CatalogGrid = styled.div`
   @media (min-width: 900px) {
     grid-template-columns: repeat(3, 1fr);
   }
-
-  @media (min-width: 1200px) {
-    grid-template-columns: repeat(4, 1fr);
-  }
 `
 
 export const Card = styled.div`
